986963c9c7ec92761998cc7b76df549177cdf313
pau.enrech <pau.enrech@promocionesfarma.com>
2021-10-14 17:09:43 +0200

    ui for item list article

--- articles/src/main/java/com/enrech/articles/presentation/ui/article_detail/view/ArticleDetailFragment.kt
+++ articles/src/main/java/com/enrech/articles/presentation/ui/article_detail/view/ArticleDetailFragment.kt
@@ -1,9 +1,30 @@
 package com.enrech.articles.presentation.ui.article_detail.view
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.enrech.articles.R
+import com.enrech.articles.databinding.FragmentArticleDetailBinding
+import com.enrech.articles.presentation.ui.article_detail.viewmodel.ArticleDetailViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class ArticleDetailFragment: Fragment(R.layout.fragment_article_detail) {
+
+    private var _binding: FragmentArticleDetailBinding? = null
+    private val binding get() = _binding!!
+
+    private val viewModel: ArticleDetailViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentArticleDetailBinding.bind(view)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
 }
--- articles/src/main/java/com/enrech/articles/presentation/ui/article_list/view/ArticleListFragment.kt
+++ articles/src/main/java/com/enrech/articles/presentation/ui/article_list/view/ArticleListFragment.kt
@@ -1,10 +1,29 @@
 package com.enrech.articles.presentation.ui.article_list.view
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.enrech.articles.R
+import com.enrech.articles.databinding.FragmentArticleListBinding
+import com.enrech.articles.presentation.ui.article_list.viewmodel.ArticleListViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class ArticleListFragment : Fragment(R.layout.fragment_article_list) {
 
+    private var _binding: FragmentArticleListBinding? = null
+    private val binding get() = _binding!!
+
+    private val viewModel: ArticleListViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentArticleListBinding.bind(view)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
