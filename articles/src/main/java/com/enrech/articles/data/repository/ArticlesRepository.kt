300ef7048b51ba7a134ca33c53593b7bf1931427
pau.enrech <pau.enrech@promocionesfarma.com>
2021-10-14 15:38:38 +0200

    create article repository

---
+++ articles/src/main/java/com/enrech/articles/data/repository/ArticlesRepository.kt
@@ -0,0 +1,28 @@
+package com.enrech.articles.data.repository
+
+import com.enrech.articles.data.datasource.ArticlesApiDatasource
+import com.enrech.articles.domain.mapper.ArticleDetailMapper
+import com.enrech.articles.domain.mapper.ArticleListMapper
+import com.enrech.articles.presentation.model.DetailedArticleVo
+import com.enrech.articles.presentation.model.SimpleArticleVo
+import com.enrech.core.data.response.Result
+import javax.inject.Inject
+
+class ArticlesRepository @Inject constructor(
+    private val articlesApiDatasource: ArticlesApiDatasource,
+    private val articleListMapper: ArticleListMapper,
+    private val articleDetailMapper: ArticleDetailMapper
+) {
+
+    suspend fun getArticlesList(): Result<List<SimpleArticleVo>> =
+        when(val result = articlesApiDatasource.getArticlesList()) {
+            is Result.Success -> Result.Success(articleListMapper.toVo(result.data))
+            is Result.Error -> result
+        }
+
+    suspend fun getArticleDetail(articleId: Int): Result<DetailedArticleVo> =
+        when(val result = articlesApiDatasource.getArticleDetail(articleId)) {
+            is Result.Success -> Result.Success(articleDetailMapper.toVo(result.data))
+            is Result.Error -> result
+        }
+}
