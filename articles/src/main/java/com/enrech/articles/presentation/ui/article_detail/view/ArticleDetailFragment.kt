3b14cdba3817308f24707b4b9e529c4bbd643530
pau.enrech <pau.enrech@promocionesfarma.com>
2021-10-14 16:35:27 +0200

    create ui base and update navigation

---
+++ articles/src/main/java/com/enrech/articles/presentation/ui/article_detail/view/ArticleDetailFragment.kt
@@ -0,0 +1,9 @@
+package com.enrech.articles.presentation.ui.article_detail.view
+
+import androidx.fragment.app.Fragment
+import com.enrech.articles.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class ArticleDetailFragment: Fragment(R.layout.fragment_article_detail) {
+}
---
+++ articles/src/main/java/com/enrech/articles/presentation/ui/article_list/view/ArticleListFragment.kt
@@ -0,0 +1,10 @@
+package com.enrech.articles.presentation.ui.article_list.view
+
+import androidx.fragment.app.Fragment
+import com.enrech.articles.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class ArticleListFragment : Fragment(R.layout.fragment_article_list) {
+
+}
