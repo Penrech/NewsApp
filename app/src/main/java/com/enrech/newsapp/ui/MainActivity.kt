182aca5544aae72940b9047bba99134181426d35
pau.enrech <pau.enrech@promocionesfarma.com>
2021-10-14 16:21:46 +0200

    start adding navigation to the app

--- app/src/main/java/com/enrech/newsapp/ui/MainActivity.kt
+++ app/src/main/java/com/enrech/newsapp/ui/MainActivity.kt
@@ -1,8 +1,11 @@
-package com.enrech.newsapp
+package com.enrech.newsapp.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.enrech.newsapp.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
